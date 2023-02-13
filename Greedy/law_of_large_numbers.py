47c3ee5f0ba54bbc517c2c3504bcf63bbfa7531c
Yongmin Park <yongmipa@student.42seoul.kr>
2023-02-13 17:46:33 +0900

    the first greedy algorithm

--- Greedy/law_of_large_numbers.py
+++ Greedy/law_of_large_numbers.py
@@ -2,6 +2,8 @@
 '''
  ---- case 1 ----
 '''
+
+
 list_size, cnt, repeat_cnt = map(int, input().split())
 
 data = list(map(int, input().split()))
@@ -31,19 +33,22 @@ print(result)
 '''
  ---- case 2 ----
 '''
+
+
 list_size, cnt, repeat_cnt = map(int, input().split())
 
 data = list(map(int, input().split()))
 
 data.sort()
-
-max = data[-1]
-sec_num = data[-2]
+max = data[-1] # 가장 큰 수
+sec_num = data[-2] # 두번째로 큰 수
 
 result = 0
-
+# 가장 큰 수가 더해지는 횟수 계산
 count = (cnt // (repeat_cnt + 1)) * repeat_cnt
 count += cnt % (repeat_cnt + 1)
 
-result += count * max
-result += (cnt - count) * sec_num
+result += count * max # 가장 큰 수 더하기
+result += (cnt - count) * sec_num # 두 번째로 큰 수 더하기
+
+print(result)
