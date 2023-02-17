900bd28f2eec052fdab176432157e373024c94e5
Yongmin Park <yongmipa@student.42seoul.kr>
2023-02-17 15:46:59 +0900

    sort by key value

--- Sorting/top_to_bottom.py
+++ Sorting/top_to_bottom.py
@@ -1,6 +1,7 @@
 # ---------------------------------------------------------- #
 '''
  ---- case 1 ----
+ 기본 정렬 방법
 '''
 
 n = int(input())
@@ -11,3 +12,22 @@ arr.sort(reverse=True)
 
 for i in arr:
 	print(i, end=' ')
+
+# ---------------------------------------------------------- #
+'''
+ ---- case 1 ----
+ 키 값으로 정렬하는 방법
+'''
+
+n = int(input())
+
+arr = []
+
+for i in range(n):
+	input_data = input().split()
+	arr.append((input_data[0], int(input_data[1])))
+
+arr.sort(key=lambda score: score[1])
+
+for student in arr:
+	print(student[0], end=' ')
